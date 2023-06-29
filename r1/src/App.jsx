a6ceec6a89f224fcaeed22bac86860e704a42da0
Lisbon <lisbon@dublin.com>
2023-06-29 14:32:38 +0300

    Initial React Commit + Racoon

---
+++ r1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Racoon from './components/003/racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React!</h1>
+        <Racoon />
+        <Racoon />
+      </header>
+    </div>
+  );
+}
+
+export default App;
