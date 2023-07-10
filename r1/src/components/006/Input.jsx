505e67d959d08032a74eb22588e3b52dcafc5714
Lisbon <lisbon@dublin.com>
2023-07-10 11:02:45 +0300

    Input practice

--- r1/src/components/006/Input.jsx
+++ r1/src/components/006/Input.jsx
@@ -1,24 +1,61 @@
 import { useState } from 'react';
-function Input() {
 
-    const [text, setText] = useState('');
+// function Input() {
 
-    const handleChange = e => {
-        if (e.target.value.length > 10) {
-            return;
-        }
-        setText(e.target.value);
+//     const [text, setText] = useState('');
+
+//     const handleChange = e => {
+//         if (e.target.value.length > 10) {
+//             return;
+//         }
+//         setText(e.target.value);
+//     }
+
+//     return (
+//         <fieldset>
+//             <legend>One text input</legend>
+//             <div>left: {10 - text.length}</div>
+//             <div>
+//                 <input type="text" value={text} onChange={handleChange} />
+//             </div>
+//             <button className="red small" onClick={_ => setText('Hello')}>Hello</button>
+//             <button className="red small" onClick={_ => setText('Bye, bye')}>Bye, bye</button>
+//             <button className="red small" onClick={_ => setText('OK')}>OK</button>
+//         </fieldset>
+//     );
+// }
+
+// function Input () {
+
+//     return (
+//       <div>
+//              <input type="text" />
+//       </div>
+//     );
+
+function Input () {
+
+const [text, setText] = useState('');
+
+const handleChange = e => {
+    // setText(e.target.value);
+    if (e.target.value.length > 10) {
+        return;
     }
+    setText(e.target.value);
+}
+
 
     return (
         <fieldset>
-            <legend>One text input</legend>
-            <div>left: {10 - text.length}</div>
+            <legend>One input text</legend>
+                <div>Input: {text}</div>
+            <div>Left: {10 - text.length}</div>
             <div>
-                <input type="text" value={text} onChange={handleChange} />
+            <input type="text" value={text} onChange={handleChange} />
             </div>
-            <button className="red small" onClick={_ => setText('Hello')}>Hello</button>
-            <button className="red small" onClick={_ => setText('Bye, bye')}>Bye, bye</button>
+            <button className='red small' onClick={_ => setText('Hello!')}>Labas!</button>
+            <button className='red small' onClick={_ => setText('Bye!')}>Viso!</button>
         </fieldset>
     );
 }
