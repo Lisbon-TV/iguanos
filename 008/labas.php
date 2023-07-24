e2e11e1978ba4d0d3a043a374bc43fb300e52fb8
Lisbon <lisbon@dublin.com>
2023-07-24 09:08:25 +0300

    PHP

--- 008/labas.php
+++ 008/labas.php
@@ -1,1 +1,27 @@
-<h1>Labas!</h1>
+<h1>Labas! (Html)</h1>
+
+<?php
+
+echo 'Labas, pasauli! (PHP)';
+
+echo '<br>';
+
+
+echo 85 * 178;
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+echo 85 * 178 / 100;
+
+?>
+
+
+<h1>Labas</h1>
+
+<?php
+
+echo '<br>';
+echo 'Rytas';
